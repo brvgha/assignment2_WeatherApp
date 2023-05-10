65e318eb5f0406b8460cf2e8a241ae5aa2e900d5
brvgha <brughafitzpatrick@gmail.com>
2023-05-10 20:23:09 +0100

    release 1

---
+++ app/controllers/WeatherCtrl.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import play.Logger;
+import play.mvc.Controller;
+import models.Reading;
+import models.Station;
+import play.Logger;
+import play.mvc.Controller;
+
+public class WeatherCtrl extends Controller {
+    public static void index(Long id)
+    {
+        Station station = Station.findById(id);
+        Logger.info("Station name = " + id);
+        render("station.html", station);
+    }
+
+    public static void addReading(Long id, int code, float temperature, float windSpeed, int pressure, int windDirection){
+        Reading reading = new Reading(code, temperature, windSpeed, pressure, windDirection);
+        Station station = Station.findById(id);
+        station.readings.add(reading);
+        station.save();
+        Logger.info("Adding Reading" + code);
+        redirect("/stations/" + id);
+    }
+
+    public static void deleteReading(Long id, Long readingid){
+        Station station = Station.findById(id);
+        Reading reading = Reading.findById(readingid);
+        Logger.info("Removing " + reading.code);
+        station.readings.remove(reading);
+        station.save();
+        reading.delete();
+        render("station.html", station);
+    }
+}
