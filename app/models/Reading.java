4517d8e6493e4e3644f43b05e981d858bb7c16d8
brvgha <brughafitzpatrick@gmail.com>
2023-05-15 19:33:15 +0100

    Release 1 - final

--- app/models/Reading.java
+++ app/models/Reading.java
@@ -14,11 +14,13 @@ public class Reading extends Model {
     public float temperature;
     public float windSpeed;
     public int pressure;
+    public float windDirection;
 
-    public Reading(int code, float temperature, float windSpeed, int pressure){
+    public Reading(int code, float temperature, float windSpeed, float windDirection,int pressure){
         this.code = code;
         this.temperature = temperature;
         this.windSpeed = windSpeed;
         this.pressure = pressure;
+        this.windDirection = windDirection;
     }
 }
